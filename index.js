8ea315a97d61c85977f8007281f147f196acb698
DevinThePancake <DevinThePancake@users.noreply.github.com>
2018-02-21 23:33:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
-const request = require("request");
-const cheerio = require("cheerio");
+const request = require('request');
+const cheerio = require('cheerio');
 
 module.exports = function FML() {
-	return new Promise((resolve, reject) => {
-		request("http://www.fmylife.com/random", (err1, body1, res) => {
-			if(err1) reject(err1);
-			let $ = cheerio.load(res);
-			let link = $("figure.text-center.visible-xs").find("a").eq(0).attr("href");
+    return new Promise((resolve, reject) => {
+        request('http://www.fmylife.com/random', (err1, body1, res) => {
+            if(err1) reject(err1);
+            let $ = cheerio.load(res);
+            let link = $('figure.text-center.visible-xs').find('a').eq(0).attr('href');
 
-			request(`http://www.fmylife.com${link}`, (err2, body2, close) => {
-				if(err2) reject(err2);
-				let o = cheerio.load(close);
-				let fml = o("p.block.hidden-xs").text();
-				if(!fml) reject("Couldn't not find any FMLs.");
-				else resolve(fml);
-			});
-		});
-	});
+            request(`http://www.fmylife.com${link}`, (err2, body2, close) => {
+                if(err2) reject(err2);
+                let o = cheerio.load(close);
+                let fml = o('p.block.hidden-xs').text();
+                if(!fml) reject('Couldn\'t not find any FMLs.');
+                else resolve(fml);
+            });
+        });
+    });
 };
